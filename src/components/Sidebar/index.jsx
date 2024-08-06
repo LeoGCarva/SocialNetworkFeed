f66f5f43ce087102768a958b460e627681caea50
Leonardo <leu1.g.carvalho@gmail.com>
2024-08-05 21:32:12 -0300

    Color change, SideBar completed

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,4 +1,5 @@
 import styles from './Sidebar.module.css';
+import { PencilSimpleLine } from 'phosphor-react';
 
 export function Sidebar(){
     return(
@@ -9,12 +10,15 @@ export function Sidebar(){
             />
 
             <div className={styles.profile}>
+                <img className={styles.avatar}src="https://github.com/LeoGCarva.png" />
+
                 <strong>Leo Carvalho</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="">
+                    <PencilSimpleLine size={20} />
                     Editar seu perfil
                 </a>
             </footer>

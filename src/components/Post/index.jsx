99667326b1c10d8dba7383930aee1eb60943c411
Leonardo <leu1.g.carvalho@gmail.com>
2024-08-05 23:18:11 -0300

    Post part done

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,10 +1,26 @@
 import styles from './Post.module.css';
 
-export function Post(props){
+export function Post(){
     return(
-    <div>
-        <strong>{props.author}</strong>
-        <p>{props.content}</p>
-    </div>
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://i.pinimg.com/736x/c0/12/bf/c012bf3b82a285f48d1e06ddbc572939.jpg"/>
+                    <div className={styles.authorInfo}>
+                        <strong>Gandalf, the Grey</strong>
+                        <span>Wizard and Hobbit admirer</span>
+                    </div>
+                </div>
+
+                <time title="05 de agosto às 22:45">1 hour ago</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Greetings 👋</p>
+                <p>So do all who live to see such times. But that is not for them to decide. All we have to decide is what to do with the time that is given us. 🧙‍♂️</p>
+                <p><a href="https://www.goodreads.com/quotes/tag/gandalf">👉 WizardThoughts.com</a></p>
+                <p><a href="">#FlyYouFools</a> <a href="">#FoolOfATook</a> <a href="">#F*ckBalrogs👎</a></p>
+            </div>
+        </article>
     );
 }
